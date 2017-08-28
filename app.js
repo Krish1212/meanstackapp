cf92e4beae6f76d3c7ec950b1eff6c561fcfe76d
krishnamoorthy <krishnamoorthy@gpslsolutions.com>
2017-08-28 17:34:31 +0530

    completed the MEAN full-stack application TASK Listing from DB Task adding Task updating Task deleting

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongojs = require('mongojs');
 
 var index = require('./routes/index');
 var tasks = require('./routes/tasks');
@@ -21,7 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.use('/', index);
 app.use('/api', tasks);
@@ -33,6 +34,7 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -41,7 +43,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error.html');
 });
 
 module.exports = app;
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,81 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://krish:krish@ds159013.mlab.com:59013/tasklist_krish',['tasks']);
+console.log(db);
+/* GET all tasks */
 router.get('/tasks', function(req, res, next) {
-  res.send('TASK API');
+  db.tasks.find(function(err, tasks){
+    if (err) {
+      return next(err);
+    }
+    res.json(tasks);
+  });
 });
+//Get single task
+router.get('/task/:id', function(req, res, next) {
+  db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)},function(err, task){
+    if (err) {
+      return next(err);
+    }
+    res.json(task);
+  });
+});
+
+//Save a task using the form
+router.post('/task', function(req, res, next) {
+  var task = req.body;
+  if(!task.title || !(task.isDone + '')){
+    res.status(400);
+    res.json({
+      "error" : "Bad data"
+    });
+  } else {
+    db.tasks.save(task, function(err, task){
+      if (err) {
+        return next(err);
+      }
+      res.json(task);
+    });  
+  }
+});
+
+//Delete a single task
+router.delete('/task/:id', function(req, res, next) {
+  db.tasks.remove({_id: mongojs.ObjectId(req.params.id)},function(err, task){
+    if (err) {
+      return next(err);
+    }
+    res.json(task);
+  });
+});
+
+//Update an existing task
+router.put('/task/:id', function(req, res, next) {
+  var task = req.body;
+  var updatedTask = {};
+
+  if(task.isDone){
+    updatedTask.isDone = task.isDone;
+  }
+  if (task.title){
+    updatedTask.title = task.title;
+  }
+
+  if(!updatedTask){
+    res.status(400);
+    res.json({
+      "error" : "Bad data"
+    });
+  } else {
+    db.tasks.update({_id: mongojs.ObjectId(req.params.id)},updatedTask,{}, function(err, task){
+      if (err) {
+        return next(err);
+      }
+      res.json(task);
+    });  
+  }
+});
+
 
 module.exports = router;
